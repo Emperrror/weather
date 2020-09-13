a71ef4e8b40e1f9cbf2067d394409999e3a31533
Ivan <ivorobyov2006@gmail.com>
2020-09-13 14:20:27 +0300

    first project

--- src/Main.java
+++ src/Main.java
@@ -67,36 +67,3 @@ public class Main {
     }
 
 }
-public class weather {
-    protected int[] temperature= new int[30];
-     String month;
-
-    weather(String month){
-        this.month=month;
-    }
-
-    public  void randWeather() {
-
-        for (int j = 0; j < 30; j++) {
-
-            temperature[j] = (int) ((Math.random() * 26)-13);
-        }
-    }
-    public void cold(){
-        int coldest= temperature[0];
-        for (int i = 1; i <30 ; i++) {
-            if (coldest>temperature[i]) {
-                coldest = temperature[i];
-            }
-        }
-        System.out.println(coldest);
-    }
-    public void displayMon(){
-
-        System.out.println("Month is "+month);
-        for (int i = 0; i < 30; i++) {
-            System.out.print(temperature[i]+"\t");
-        }
-    }
-
-}

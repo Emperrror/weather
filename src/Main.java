64eddec3b8f3c577ae8ee93157ed16f01271f2cf
Ivan <ivorobyov2006@gmail.com>
2020-09-19 16:47:32 +0300

    done.

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        weather Jan = new weather("January");
-        weather Feb = new weather("February");
-        weather Mar = new weather("March");
-        weather Apr = new weather("April");
-        weather May = new weather("May");
-        weather Jun = new weather("June");
-        weather Jul= new weather("July");
-        weather Aug = new weather("August");
-        weather Sep = new weather("September");
-        weather Oct = new weather("October");
-        weather Nov = new weather("November");
-        weather Dec = new weather("December");
+      weather Jun = new weather("June");
+      weather Jul = new weather("July");
+      Jun.calendar();
+      Jul.calendar();
+        System.out.println(Jun.average());
+        System.out.println(Jul.average());
 
-        Jan.randWeather();
-        Feb.randWeather();
-        Mar.randWeather();
-        Apr.randWeather();
-        May.randWeather();
-        Jun.randWeather();
-        Jul.randWeather();
-        Aug.randWeather();
-        Sep.randWeather();
-        Oct.randWeather();
-        Nov.randWeather();
-        Dec.randWeather();
-
-        Scanner scan = new Scanner(System.in);
-        try {
-            System.out.println("What do you want to know:\n1.");
-            byte answer = scan.nextByte();
-            if (answer == 1) {
-                Jan.displayMon();
-                Feb.displayMon();
-                Mar.displayMon();
-                Apr.displayMon();
-                May.displayMon();
-                Jun.displayMon();
-                Jul.displayMon();
-                Aug.displayMon();
-                Sep.displayMon();
-                Oct.displayMon();
-                Nov.displayMon();
-                Dec.displayMon();
-            // }
-            //else if (answer==2){
-                Jan.cold();
-                Feb.cold();
-                Mar.cold();
-                Apr.cold();
-                May.cold();
-                Jun.cold();
-                Jul.cold();
-                Aug.cold();
-                Sep.cold();
-                Oct.cold();
-                Nov.cold();
-                Dec.cold();
-
-
-            }
-        }catch (Exception e) {
-        }
     }
-
 }
--- src/weather.java
+++ src/weather.java
@@ -1,33 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class weather {
-    protected int[] temperature= new int[30];
-     String month;
+int [] temperature = new int[30];
+String month;
 
-    weather(String month){
-        this.month=month;
-    }
+ weather(String month){
+    month = this.month;
+     for (int i = 0; i < 30; i++) {
+         Random random = new Random();
+         temperature[i] = random.nextInt(20);
+     }
+   }
+   public void calendar(){
+        for (int i = 0; i < 30 ; i++) {
+            System.out.print(temperature[i] + "\t");
 
-    public  void randWeather() {
-
-        for (int j = 0; j < 30; j++) {
-
-            temperature[j] = (int) ((Math.random() * 26)-13);
         }
+       System.out.println();
     }
-    public void cold(){
-        int coldest= temperature[0];
-        for (int i = 1; i <30 ; i++) {
-            if (coldest>temperature[i]) {
-                coldest = temperature[i];
+    public int average(){
+     int av1=0;
+     int av3=0;
+     int av2=0;
+        for (int i = 0; i < 30; i++) {
+            av1 += temperature[i];
+        }
+        av1/=30;
+        av2 = Math.abs(temperature[0]- av1);
+        for (int i = 1; i < 30; i++) {
+            if(Math.abs(temperature[i]-av1)>av2){
+                av2 =Math.abs(temperature[i]-av1);
+                av3 = i;
             }
         }
-        System.out.println(coldest);
-    }
-    public void displayMon(){
-
-        System.out.println("Month is "+month);
-        for (int i = 0; i < 30; i++) {
-            System.out.print(temperature[i]+"\t");
+        System.out.println("Средняя температура: "+ av2);
+        return av3;
         }
-    }
-
 }
